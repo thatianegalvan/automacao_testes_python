2b44d37ca923f974d31f8eaea5306b927d0a5af1
Thatiane Galvan <thatianegalvan@gmail.com>
2026-09-10 20:42:13 -0300

    add os chrome.options

--- tests/fixtures/driver.py
+++ tests/fixtures/driver.py
@@ -19,6 +19,10 @@ def driver_func():
     # ✅ Perfil limpo e isolado (ESSENCIAL!!!)
     user_data_dir = tempfile.mkdtemp()
     chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     # ✅ Desativa Password Manager convencional
     prefs = {
