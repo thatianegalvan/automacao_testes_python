8197ddc7ce43e539c7b76dd19220f4b0d4a6a625
Thatiane Galvan <thatianegalvan@gmail.com>
2026-09-12 16:24:58 -0300

    test: Ajustes 2  na classe removeFromCartTrasaction

--- tests/transactions/remove_from_cart_transaction.py
+++ tests/transactions/remove_from_cart_transaction.py
@@ -1,8 +1,12 @@
-from tests.pages.inventory_page import InventoryPage
+
 from guara.transaction import AbstractTransaction
 from tests.pages.inventory_page import InventoryPage
 
 class RemoveFromCartTransaction(AbstractTransaction):
+
+    def __init__(self, driver):
+        super().__init__()
+        self.driver = driver
 
     def do(self):
 
