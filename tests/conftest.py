27c545b52237d98abb4f2bedb9c8b45a711c3473
Thatiane Galvan <thatianegalvan@gmail.com>
2026-09-12 13:17:26 -0300

    test: criando novo arquivo conftest.py para rodar a pipeline action

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import tempfile
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def driver_func():
+    chrome_options = Options()
+    user_data_dir = tempfile.mkdtemp()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False,
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument("--disable-features=PasswordLeakDetection")
+    chrome_options.add_argument("--safebrowsing-disable-leak-detection")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    return driver
+
+@pytest.fixture
+def driver():
+    driver_instance = driver_func()
+    yield driver_instance
+    driver_instance.quit()
